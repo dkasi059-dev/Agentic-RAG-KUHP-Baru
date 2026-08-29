7389bc776531e7c508c1c997f932cd7b19972df6
dkasi059-dev <dkasi059@gmail.com>
2026-08-29 08:14:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 # ================================
 llm = ChatOpenAI(
     model="cohere/north-mini-code:free",
-    temperature=0,
+    temperature=0.3,
     openai_api_key=st.secrets["API_OR"],
     openai_api_base="https://openrouter.ai/api/v1"
 )
