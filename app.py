ea84d69b072b30100b4938aeec168b6b4ed6aa30
dkasi059-dev <dkasi059@gmail.com>
2026-08-29 19:53:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 # ================================
 # 🔮 Setup LLM
 # ================================
-llm = ChatGroq(
-    model="qwen/qwen3.6-27b",        
+llm = ChatOpenAI(
+    model="inclusionai/ling-3.0-flash-fin:free",
     temperature=0,
-    groq_api_key=st.secrets["API_GROQ"]
+    openai_api_key=st.secrets["API_OR"],
+    openai_api_base="https://openrouter.ai/api/v1"
 )
 
 # ================================
