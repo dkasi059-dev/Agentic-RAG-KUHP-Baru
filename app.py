b1ce131114d0f4776a0755e81c888667233feb0b
dkasi059-dev <dkasi059@gmail.com>
2026-08-30 20:56:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
 # 🔮 Setup LLM
 # ================================
 llm = ChatOpenAI(
-    model="poolside/laguna-s-2.1:free",
+    model="cohere/north-mini-code:free",
     temperature=0,
     openai_api_key=st.secrets["API_OR"],
     openai_api_base="https://openrouter.ai/api/v1"
