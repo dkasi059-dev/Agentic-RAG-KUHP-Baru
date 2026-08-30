193e6443e60c7af39942bbcbfe74181c8a81dfa7
dkasi059-dev <dkasi059@gmail.com>
2026-08-30 13:14:03 +0700

    Update ui_chat.py

--- ui_chat.py
+++ ui_chat.py
@@ -197,11 +197,11 @@ st.markdown(
 
 
     /* ========================================================
-       CHAT MESSAGE BOX - HIJAU TERANG MENYALA ESTETIK
+       CHAT MESSAGE BOX - MERAH TERANG MENYALA ESTETIK
     ======================================================== */
 
     [data-testid="stChatMessage"] {
-        background: linear-gradient(145deg, #22ff00, #cc0033) !important;
+        background: linear-gradient(145deg, #f00226, #cc0033) !important;
         border: 1px solid rgba(255, 0, 64, 0.7) !important;
         box-shadow:
             0 0 25px rgba(255, 0, 64, 0.6),
